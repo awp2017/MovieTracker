8ccb0739e4a3689f980f802e55b7e60f716aab31
matei <matei>
2018-01-12 20:48:37 +0200

    add urls

--- movie_tracker/urls.py
+++ movie_tracker/urls.py
@@ -3,5 +3,12 @@ from django.conf.urls import url
 from views import *
 
 urlpatterns = [
-    url(r'^movies/', MoviesIndex.as_view(), name='movies_index')
+    url(r'^movies/', MoviesIndex.as_view(), name='movies_index'),
+    url(r'^/movies/(?P<pk>[0-9]+)/$', MoviesIndex.as_view(), name='movies_show'),
+    url(r'^/movies/(?P<pk>[0-9]+)/comments$', MoviesIndex.as_view(), name='movie_comments'),
+    url(r'^/movies/(?P<pk_movie>[0-9]+)/actors/$', MoviesIndex.as_view(), name='movie_actors'),
+    url(r'^/actors/', MoviesIndex.as_view(), name='actors_index'),
+    url(r'^/actors/(?P<pk>[0-9]+)/$', MoviesIndex.as_view(), name='actors_show'),
+    url(r'^/users/(?P<pk>[0-9]+)/watched$', MoviesIndex.as_view(),name='movies_watched'),
 ]
+
