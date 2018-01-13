c39301794c29fcfa492ecc01adf6bb8e1101b0ba
Plesu Oana <plesu.oana@gmail.com>
2018-01-13 11:53:09 +0200

    added authentification views

--- movie_tracker/urls.py
+++ movie_tracker/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 
 import views.actors as actors
 import views.test as test
+import views.signup as signup
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^movies/', actors.Index.as_view(), name='movies_index'),
@@ -12,5 +14,8 @@ urlpatterns = [
     url(r'^actors/(?P<pk>[0-9]+)/$', actors.Index.as_view(), name='actors_show'),
     url(r'^watched$', actors.Index.as_view(),name='movies_watched'),
     url(r'^test/$', test.Test.as_view(), name='test'),
+    url(r'^signup/$', signup.SignUp.as_view(), name='signup'),
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, {'next_page': 'movies_index'}, name='logout'),
 ]
 
