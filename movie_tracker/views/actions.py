d33f56dd13cb4259e3fb6ffb54069374fdfeaaca
matei <matei>
2018-01-13 13:03:51 +0200

    add search bar

--- movie_tracker/views/actions.py
+++ movie_tracker/views/actions.py
@@ -1,16 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import pdb
+
 from django.shortcuts import redirect
 from django.urls import reverse
 
 
 def search(request):
-    if request.GET.get('actor'):
-        return redirect(reverse('actors_index') + '?actor={0}'.format(request.GET.get('actor')))
+    option = request.GET.get('option')
+    value = request.GET.get('search', '')
 
-    if request.GET.get('movie'):
-        return redirect(reverse('movies_index') + '?movie={0}'.format(request.GET.get('movie')))
-
-    if request.GET.get('keyword'):
-        return redirect(reverse('movies_index') + '?keyword={0}'.format(request.GET.get('keyword')))
+    if option == 'actor':
+        return redirect(reverse('actors_index') + '?actor={0}'.format(value))
+    elif option == 'movie':
+        return redirect(reverse('movies_index') + '?movie={0}'.format(value))
+    elif option == 'keyword':
+        return redirect(reverse('movies_index') + '?keyword={0}'.format(value))
