4990989815c19b01ca0639aa35cc955185ed2c3f
iancu <robert.iancu@protonmail.com>
2018-01-13 13:04:36 +0200

    add comments view edit index and show view

--- movie_tracker/urls.py
+++ movie_tracker/urls.py
@@ -8,7 +8,7 @@ import views.test as test
 urlpatterns = [
     url(r'^movies/$', movies.Index.as_view(), name='movies_index'),
     url(r'^movies/(?P<pk>[0-9]+)/$', movies.MovieDetailView.as_view(), name='movies_show'),
-    url(r'^movies/(?P<pk>[0-9]+)/comments$', actors.Index.as_view(), name='movie_comments'),
+    url(r'^movies/(?P<pk>[0-9]+)/comments$', movies.CommentsIndex.as_view(), name='movie_comments'),
     url(r'^movies/(?P<pk_movie>[0-9]+)/actors/$', actors.Index.as_view(), name='movie_actors'),
     url(r'^actors/$', actors.Index.as_view(), name='actors_index'),
     url(r'^actors/(?P<pk>[0-9]+)/$', actors.Show.as_view(), name='actors_show'),
--- movie_tracker/views/movies.py
+++ movie_tracker/views/movies.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, View
 from movie_tracker.models import *
 from django.core.paginator import Paginator
 
@@ -17,3 +17,14 @@ class MovieDetailView(DetailView):
     template_name = 'movies/show.html'
     model = Movie
     context_object_name = 'movie'
+
+class CommentsIndex(View):
+    def get(self, request, pk):
+        movie = Movie.objects.get(id=pk)
+        comments = Comment.objects.filter(movie=pk)
+        return render(
+            request,
+            'movies/comments.html',
+            context={'comments': comments,
+                     'movie': movie}
+        )
